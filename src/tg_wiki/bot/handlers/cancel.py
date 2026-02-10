c64f8dc6a48b4f9db194693e9f9868936627c99b
arssmol1029 <arssmol1029@gmail.com>
2026-02-10 20:56:21 +0300

    remove unnecessary imports

--- src/tg_wiki/bot/handlers/cancel.py
+++ src/tg_wiki/bot/handlers/cancel.py
@@ -3,9 +3,6 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
-from tg_wiki.services.wiki_service import search_articles
-from tg_wiki.bot.handlers.search import search_handler
-from tg_wiki.bot.states import SearchState
 
 router = Router()
 
