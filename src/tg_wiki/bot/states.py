a3d0a58e8e7088fbebe6e1dbb8a988abbc9eba39
arssmol1029 <arssmol1029@gmail.com>
2026-02-25 21:32:00 +0300

    Remove files from main branch

--- src/tg_wiki/bot/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class SearchState(StatesGroup):
-    waiting_for_query = State()
-
-
-class SettingsEditState(StatesGroup):
-    waiting_for_query = State()
